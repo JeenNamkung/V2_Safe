62886cb2a51171040176307c608db150290dd6a5
Jeen Namkung <nkjeen3876@gmail.com>
2024-02-04 16:09:16 -0800

    1

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const db_utils = include('database/db_utils');
 const db_users = include('database/users');
 const success = db_utils.printMySQLVersion();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 const app = express();
 
